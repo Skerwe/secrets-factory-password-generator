5e3ce7071aa4e98d641b4be59ef3ef248c3a8592
Quintin Henn <quintin.henn@gmail.com>
2019-09-09 15:39:49 +0200

    Fix depricated OptionBuilder to use Option.builder

--- src/main/java/za/web/skerwe/factory/SecretsFactory.java
+++ src/main/java/za/web/skerwe/factory/SecretsFactory.java
@@ -1,3 +1,21 @@
+/**
+  Secrets factory - a password generator
+  Copyright (C) 2019  Quintin Henn
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package za.web.skerwe.factory;
 
 import za.web.skerwe.factory.secrets.PassayGenerator;
@@ -7,7 +25,6 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.HelpFormatter;
 
 import org.apache.commons.cli.ParseException;
@@ -15,7 +32,7 @@ import org.apache.commons.cli.ParseException;
 /**
  * @author Quintin henn
  * @since 04.09.2019
- * @version 04.09.2019
+ * @version 09.09.2019
  */
 public class SecretsFactory {
 
@@ -29,17 +46,17 @@ public class SecretsFactory {
     } else {
       try {
         generator.resetConfiguration();
-        
+
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = parser.parse(getOptions(), args);
-  
+
         if (cmd.hasOption("help")) {
           HelpFormatter formatter = new HelpFormatter();
           formatter.printHelp("ant", getOptions());
         }
         if (cmd.hasOption("version")) {
         }
-  
+
         if (cmd.hasOption("l")) {
           generator.configureLowerCase(Integer.valueOf(cmd.getOptionValue("l")));
           generator.setUseLowerCase(true);
@@ -69,29 +86,29 @@ public class SecretsFactory {
     Option help = new Option("help", "print this message");
     Option version = new Option("version", "print the version information and exit");
 
-    Option lowerCase = OptionBuilder
-      .withArgName("value")
+    Option lowerCase = Option.builder("l")
+      .argName("value")
       .hasArg()
-      .withDescription("number of lower case characters")
-      .create("l");
-    
-    Option upperCase = OptionBuilder
-      .withArgName("value")
+      .desc("number of lower case characters")
+      .build();
+
+    Option upperCase = Option.builder("u")
+      .argName("value")
       .hasArg()
-      .withDescription("number of upper case character")
-      .create("u");
-    
-    Option digits = OptionBuilder
-      .withArgName("value")
+      .desc("number of upper case character")
+      .build();
+
+    Option digits = Option.builder("d")
+      .argName("value")
       .hasArg()
-      .withDescription("numbers of digits")
-      .create("d");
-    
-    Option specials = OptionBuilder
-      .withArgName("value")
+      .desc("numbers of digits")
+      .build();
+
+    Option specials = Option.builder("s")
+      .argName("value")
       .hasArg()
-      .withDescription("number of special characters")
-      .create("s");
+      .desc("number of special characters")
+      .build();
 
     Options options = new Options();
     options.addOption(help);
@@ -102,5 +119,12 @@ public class SecretsFactory {
     options.addOption(specials);
 
     return options;
+  }
+
+  private String getLicense() {
+    return String "<program>  Copyright (C) <year>  <name of author>
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.";
   }
 }
